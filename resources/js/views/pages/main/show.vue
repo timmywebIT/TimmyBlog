0e14a6b69fdb30228d3c7c9535642db29bf4c8b3
timmy-ctrl <timmygithub@gmail.com>
2025-08-28 17:13:32 +0800

    add arrow

--- resources/js/views/pages/main/show.vue
+++ resources/js/views/pages/main/show.vue
@@ -1,18 +1,27 @@
 <template>
- <h1 class="text-black"></h1>
-    <p>{{postStore.post.content}}</p>
-
+    <div class="container">
+        <div>
+            <h1 class="show-title">{{ postStore.post.title }}</h1>
+        </div>
+        <div>
+            <router-link :to="{name: 'main.index'}">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+            </svg>
+            </router-link>
+        </div>
+        <div class="show-p">
+            <p>{{postStore.post.content}}</p>
+        </div>
+    </div>
 </template>
 <script setup>
 import {onMounted} from "vue"
 import {usePostStore} from "../../../stores/post.js";
-import { useRoute } from 'vue-router';
 
 defineOptions({
     name: "Show",
 })
-
-const route = useRoute()
 
 onMounted(() => {
     postStore.getPost()
@@ -23,5 +32,20 @@ const postStore = usePostStore()
 </script>
 
 <style scoped>
+
+.container {
+    text-align: center;
+}
+
+.show-title {
+    font-size: 2em;
+    font-weight: bold;
+    margin-top: 3em;
+}
+.show-p {
+    margin-top: 2em;
+    font-size: 20px;
+}
+
 
 </style>
