45d1080b760574f1a56d6ec9a66cf4f9699815ec
timmy-ctrl <timmygithub@gmail.com>
2025-08-31 22:06:09 +0800

    Refactoring sidebar

--- resources/js/layouts/AdminLayout.vue
+++ resources/js/layouts/AdminLayout.vue
@@ -1,22 +1,15 @@
 <template>
-    <div>
-        <section>
-            <header class="header-title">
-                Admin dashboard
-            </header>
-        </section>
+    <div class="">
         <div class="container-aside">
             <section class="content-aside">
-                <aside class="menu-aside">
-                    <router-link :to="{name: 'admin.dashboard.index'}" class="menu-item">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
+                <aside class="menu-aside w-fit h-20 rounded-lg text-white bg-black  p-5 m-3 flex flex-row items-center gap-2">
+                    <router-link :to="{name: 'admin.dashboard.index'}" class="menu-item flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
                         <span>Главная</span>
                     </router-link>
-                    <router-link :to="{name: 'admin.posts.index'}" class="menu-item">
+                    <router-link :to="{name: 'admin.posts.index'}" class="menu-item flex items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -24,7 +17,7 @@
                         </svg>
                         <span>Посты</span>
                     </router-link>
-                    <router-link to="" class="menu-item">
+                    <router-link to="" class="menu-item flex items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -32,7 +25,7 @@
                         </svg>
                         <span>Категории</span>
                     </router-link>
-                    <router-link :to="{name: 'admin.tags.index'}" class="menu-item">
+                    <router-link :to="{name: 'admin.tags.index'}" class="menu-item flex items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -41,18 +34,11 @@
                         </svg>
                         <span>Теги</span>
                     </router-link>
-                    <router-link to="" class="menu-item">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"/>
-                        </svg>
-                        <span>Пользователи</span>
-                    </router-link>
                 </aside>
             </section>
         </div>
     </div>
+
 </template>
 
 <script setup>
@@ -64,45 +50,5 @@ defineOptions({
 </script>
 
 <style scoped>
-
-.header-title {
-    font-size: 22px;
-    font-weight: bold;
-    margin-left: 0.5em;
-
-}
-
-.container-aside {
-    width: 18%;
-    height: 100%;
-    background-color: #FBCEB1;
-    position: fixed;
-    left: 0;
-}
-
-.content-aside {
-    width: 50%;
-    margin-left: 1em;
-    padding: 1em;
-}
-
-.menu-aside {
-    display: flex;
-    flex-direction: column;
-    font-size: 20px;
-    gap: 1.2em;
-}
-
-.menu-item {
-    display: flex;
-    align-items: center;
-    gap: 0.4em;
-}
-
-.menu-item svg {
-    width: 20px;
-    height: 20px;
-    flex-shrink: 0;
-}
 
 </style>
